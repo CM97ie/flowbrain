3fc46a5bead284eb2f9612a8d31aa1a570757243
CM97ie <153494100+CM97ie@users.noreply.github.com>
2026-04-27 13:56:25 +0100

    Update app.js

--- app.js
+++ app.js
@@ -568,7 +568,7 @@ renderHomeReminders();
 updateHomeStats();
 renderWinddown();
 
-// Register service worker
+
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('sw.js').catch(() => {});
